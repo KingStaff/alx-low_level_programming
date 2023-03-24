fef0e771b92d018ac6a63c31ec7045f4f1dc03b6
Karim Awudulai <karimawudulai@gmail.com>
2023-03-24 09:16:38 -0700

    C - Functions, nested loops

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * print_times_table - prints the n times table, starting with 0.
  *
@@ -9,41 +10,42 @@
 
 void print_times_table(int n)
 {
-	int row, col;
+    int row, col;
 
-	if (n < 15 || n < 0)
-
-	for (row = 0; row <= n; row++)
-	{
-		for (col = 0; col <= n; col++)
-		{
-			if (col == 0)
-				_putchar(((row * col) % 10) + '0');
-			else if ((row * col) <= 9)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar((row * col) + '0');
-			}
-			else if ((row * col) <= 99)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(((row * col) / 10) + '0');
-				_putchar(((row * col) % 10) + '0');
-			}
-			else
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(((row * col) / 100) + '0');
-				_putchar((((row * col) / 10) % 10) + '0');
-				_putchar(((row * col) % 10) + '0');
-			}
-		}
-		_putchar('\n');
-	}
+    if (n >= 0 && n <= 15)
+    {
+        for (row = 0; row <= n; row++)
+        {
+            for (col = 0; col <= n; col++)
+            {
+                if (col == 0)
+                    _putchar(row * col + '0');
+                else if (row * col <= 9)
+                {
+                    _putchar(',');
+                    _putchar(' ');
+                    _putchar(' ');
+                    _putchar(' ');
+                    _putchar(row * col + '0');
+                }
+                else if (row * col <= 99)
+                {
+                    _putchar(',');
+                    _putchar(' ');
+                    _putchar(' ');
+                    _putchar((row * col) / 10 + '0');
+                    _putchar((row * col) % 10 + '0');
+                }
+                else
+                {
+                    _putchar(',');
+                    _putchar(' ');
+                    _putchar((row * col) / 100 + '0');
+                    _putchar(((row * col) / 10) % 10 + '0');
+                    _putchar((row * col) % 10 + '0');
+                }
+            }
+            _putchar('\n');
+        }
+    }
 }
